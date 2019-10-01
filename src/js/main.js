47c40ce9188882d04df9ff535cd9c295c8031c71
Ula Krawczyk <ula.krawczyk7@gmail.com>
2019-10-01 15:41:37 +0200

    chore: simple logic in js

--- src/js/main.js
+++ src/js/main.js
@@ -3,20 +3,36 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
 
-// place your code below
+const buttonAdd = document.querySelector(".drink__button--1-js");
+const buttonSub = document.querySelector(".drink__button--2-js");
+const glassNumber = document.querySelector(".drink__number--js");
+let number = 0;
 
+buttonAdd.addEventListener("click", function () {
+  number++;
+  if (number >= 100) {
+    alert("You've drunk too much today:)");
+  } else {
+    glassNumber.innerHTML = number;
+  }
+});
 
-console.log(`Hello world!`);
-
-
+buttonSub.addEventListener("click", (e) => {
+  number--;
+  if (number <= 0) {
+    alert("You haven't drunk anything today:)");
+  } else {
+    glassNumber.innerHTML = number;
+  }
+});
